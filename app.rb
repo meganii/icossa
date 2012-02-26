b58b0ba7e8a552af53d113cc1bd276e81a5c48e8
meganii <yuhei24@gmail.com>
2012-02-27 00:25:25 +0900

    add views/

--- app.rb
+++ app.rb
@@ -10,10 +10,27 @@ class Post
 end
 
 get '/' do
-  Post.all.map{|r| "#{r.id}, #{r.user},#{r.create_at} <br />"} 
+  @post = Post.all.map{|r| "#{r.id}, #{r.user},#{r.create_at} <br />"} 
+  erb :index
+end
+
+get '/:id' do
+  @post = Post.get(params[:id])
+  erb :post
 end
 
 get '/create' do
   post = Post.create(:user => params[:user],:create_at => Time.now)
   "#{params[:user]}"
+end
+
+post '/create' do
+  post = Post.new(:user => params[:user])
+  if post.save
+    status 201
+    redirect '/'+post.id.to_s
+  else
+    status 412
+    redirect '/'
+  end
 end
